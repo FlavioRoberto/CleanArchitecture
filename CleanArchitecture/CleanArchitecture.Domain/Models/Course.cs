9d790391bac49b46f9851a052f9a03ec015b0d5f
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2021-07-31 16:52:10 -0300

    Adding Course Domain Model

---
+++ CleanArchitecture/CleanArchitecture.Domain/Models/Course.cs
@@ -0,0 +1,18 @@
+﻿namespace CleanArchitecture.Domain.Models
+{
+    public class Course
+    {
+        public long Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public string ImageUrl { get; private set; }
+
+        protected Course() { }
+        public Course(string name, string description, string imageUrl)
+        {
+            Name = name;
+            Description = description;
+            ImageUrl = imageUrl;
+        }
+    }
+}
