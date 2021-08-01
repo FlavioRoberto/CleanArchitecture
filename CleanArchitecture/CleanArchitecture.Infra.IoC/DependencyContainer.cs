3c28230f31790c80d3296669a07933250971273b
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2021-08-01 13:47:08 -0300

    Configuring Presentation Layer for Dependency Container

---
+++ CleanArchitecture/CleanArchitecture.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,20 @@
+﻿using CleanArchitecture.Application.Interfaces;
+using CleanArchitecture.Application.Services;
+using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Infra.Data.Repository;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Application A
+            services.AddTransient<ICourseService, CourseService>();
+
+            //Infra.Data Layer
+            services.AddScoped<ICourseRepository, CourseRepository>();
+        }
+    }
+}
--- CleanArchitecture/CleanArchitecture.MVC/Startup.cs
+++ CleanArchitecture/CleanArchitecture.MVC/Startup.cs
@@ -1,18 +1,13 @@
 using CleanArchitecture.Infra.Data.Context;
+using CleanArchitecture.Infra.IoC;
 using CleanArchitecture.MVC.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CleanArchitecture.MVC
 {
@@ -40,7 +35,10 @@ namespace CleanArchitecture.MVC
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+         
             services.AddControllersWithViews();
+
+            RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -72,6 +70,11 @@ namespace CleanArchitecture.MVC
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
+        }
+
+        private static void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
         }
     }
 }
